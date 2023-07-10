560d77b848ad6977477753139c2b286db91e0a86
ntv-python <139030853+ntv-python@users.noreply.github.com>
2023-07-10 22:57:25 +0700

    Update Code Base (PYTHON2).py

--- Code Base (PYTHON2).py
+++ Code Base (PYTHON2).py
@@ -20,7 +20,7 @@ years = my_data['Year'].unique()
 
 custom_colors = ['#331177', '#F9C70C']  # Custom colors: Indigo and Amber
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MORPH])
-
+server = app.server
 
 # Data for Pie chart
 gdp_sum_by_status_year = my_data.groupby(['Year', 'Status'])['GDP'].sum().reset_index()
